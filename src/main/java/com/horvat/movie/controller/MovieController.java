f9fe9112da636dab3623c90915f8025696aeca05
JosipHorvat <josiph988@gmail.com>
2020-12-28 23:11:43 +0100

    getMovieBySearchCriteria last touch :)

--- src/main/java/com/horvat/movie/controller/MovieController.java
+++ src/main/java/com/horvat/movie/controller/MovieController.java
@@ -4,9 +4,12 @@ import com.horvat.movie.model.Movie;
 import com.horvat.movie.repository.MovieRepository;
 import com.horvat.movie.service.IMovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,8 +29,22 @@ public class MovieController {
 
 
 
-    @GetMapping("/movie")
+    @GetMapping("/movies")
+    // localhost:8080/api/movie
     public List<Movie> getAllMovies(){
        return iMovieService.getAllMovies();
     }
+
+
+    @GetMapping("/movie/")
+    // EXAMPLE FOR POSTMAN:
+    // localhost:8080/api/movie/?search=title:
+    //localhost:8080/api/movie/?search=movieCategory:Horror
+    //localhost:8080/api/movie/?search=length:100,movieCategory:Horror
+   public ResponseEntity<List<Movie>> getMovieBySearchCriteria(@RequestParam String search){
+
+        List<Movie> list = iMovieService.findAllBySpecification(search);
+        return new ResponseEntity<List<Movie>>(list, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/horvat/movie/service/MovieService.java
+++ src/main/java/com/horvat/movie/service/MovieService.java
@@ -3,12 +3,15 @@ package com.horvat.movie.service;
 import com.horvat.movie.model.Movie;
 import com.horvat.movie.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
-public class MovieService implements IMovieService{
+public class MovieService implements IMovieService {
 
     @Autowired
     private MovieRepository movieRepository;
@@ -21,6 +24,31 @@ public class MovieService implements IMovieService{
 
     @Override
     public List<Movie> findAllBySpecification(String search) {
-        return null;
+        //https://www.baeldung.com/rest-api-search-language-spring-data-specifications
+
+        MovieSpecificationBuilder builder = new MovieSpecificationBuilder();
+        Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),", Pattern.UNICODE_CHARACTER_CLASS);
+        // PATERN FOR ENGLISH LANGUAGE:  Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+        Matcher matcher = pattern.matcher(search + ",");
+
+        while (matcher.find()) {
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+          /*
+         PATERNA
+        Specification<Movie> movieSpecification = builder
+                .with("name", ":", "Friday 13")
+                .with("movieCategory" ,":","Drama")
+                .with("length", ":", "180")
+                .build();
+         */
+
+        Specification<Movie> movieSpec = builder.build();
+
+
+        return movieRepository.findAll(movieSpec);
+        }
+
+
     }
-}
+
